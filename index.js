4393648249e73ce02e6fe7983b877d3e81671739
Danny Lee <siewkhee1990@hotmail.com>
2022-04-11 01:58:44 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middlewares
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", frontEndUrl);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, DELETE, OPTIONS, PATCH"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, x-access-token, Cookie, Content-Type, access_token, Accept"
+  );
+  next();
+});
+
+//this will catch any route that doesn't exist
+app.get("*", (req, res) => {
+  res.status(200).json("Endpoint Successful!");
+});
+
+app.listen(port, () => {
+  console.log("I am listening to port:", port);
+});
